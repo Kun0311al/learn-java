7ef0632664f2593ccbe339dcff732f73bee50e35
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2021-12-11 22:59:57 +0530

    Variables in java
    how to declare the variable in java and the different variable types in java.

---
+++ Variable.java
@@ -0,0 +1,14 @@
+public class Variable {
+    public static void main(String[] args){
+        int a = 10;
+        String name = "Kunal";
+        boolean b = true;
+        float c = 45.45f;
+        char d = 'c';
+        System.out.println(a);
+        System.out.println(name);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
