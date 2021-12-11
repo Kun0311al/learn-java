a15060464ed6081f4d286a6e3be8f06a3b13c151
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2021-12-11 23:18:51 +0530

    adding arithmatic operators

---
+++ Operators.java
@@ -0,0 +1,14 @@
+public class Operators {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 35;
+        //Arithmatic operators
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println(++a);
+        System.out.println(--b);
+    }
+}
