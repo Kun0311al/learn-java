221a2c29b813abeffa63ac7a14fae0a97a9eeab9
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2021-12-11 23:56:52 +0530

    adding if else statement

---
+++ If statement/IfElse.java
@@ -0,0 +1,12 @@
+public class IfElse {
+    public static void main(String[] args){
+        int a = 100;
+        int b= 24;
+        if(a<b){
+            System.out.println("a is smaller than b.");
+        }
+        else{
+            System.out.println("a is greater than b");
+        }
+    }
+}
